755c0f14884592cb567b5c8b3b7707c4ff96cfdc
VitorOtero <vitorotero@ip-172-17-35-64.sa-east-1.compute.internal>
2019-02-25 13:38:09 -0300

    ajuste na loading view

---
+++ app/src/main/java/br/com/tecapp/personproject/ui/photos/detail/DetailPhotoActivity.kt
@@ -0,0 +1,13 @@
+package br.com.tecapp.personproject.ui.photos.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.tecapp.personproject.R
+
+class DetailPhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_photo_screen)
+    }
+}
--- app/src/main/java/br/com/tecapp/personproject/utils/BindingAdapters.kt
+++ app/src/main/java/br/com/tecapp/personproject/utils/BindingAdapters.kt
@@ -1,18 +1,53 @@
 package br.com.tecapp.personproject.utils
 
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class BindingAdapters {
 
     companion object {
 
-        @BindingAdapter("android:loadImageWithGlide")
+        @BindingAdapter("android:loadImageWithGlide", "android:progressViewGlide")
         @JvmStatic
-        fun loadImage(view: AppCompatImageView, url: String) {
+        fun loadImage(view: AppCompatImageView, url: String, progressView: ProgressBar?) {
+            progressView?.let {
+                it.visibility = View.VISIBLE
+            }
             Glide.with(view)
                 .load(url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return true
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressView?.let {
+                            it.visibility = View.GONE
+                        }
+
+                        view.setImageDrawable(resource!!)
+                        return true
+                    }
+                })
                 .centerCrop()
                 .into(view)
         }
